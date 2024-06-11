5f07e94bec4c13a510f60818e6486cde359bd368
Ahmed Raza <231564@students.au.edu.pk>
2024-06-11 10:16:37 +0500

    Add files via upload

---
+++ OOP PROJECT.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include"Person.h"
+#include"Customer.h"
+#include"Employee.h"
+using namespace std;
+
+int main() {
+    Employee c;
+    c.Employee_Menu();
+    //c.view_from_file();
+
+
+    return 0;
+}
